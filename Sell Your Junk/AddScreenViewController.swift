25a956035b1fad339c71361b1e24923348c3d483
Xavier Triplett <Xavier Triplett>
2019-04-26 20:39:39 -0700

    Add screen

---
+++ Sell Your Junk/AddScreenViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddScreenViewController.swift
+//  Sell Your Junk
+//
+//  Created by Xavier Triplett on 4/26/19.
+//  Copyright © 2019 Xavier Triplett. All rights reserved.
+//
+
+import UIKit
+
+class AddScreenViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var categoryField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
