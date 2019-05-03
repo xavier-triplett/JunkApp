1b8a1d23d502db36fad5e2ba2af55b5fa69cb87a
Xavier Triplett <Xavier Triplett>
2019-05-03 12:08:07 -0700

    Added segue

--- Sell Your Junk/BuyTableViewController.swift
+++ Sell Your Junk/BuyTableViewController.swift
@@ -80,15 +80,22 @@ class BuyTableViewController: UITableViewController, DataHandler {
         return true
     }
     */
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToBuyView", sender: self)
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if let indexPath = tableView.indexPathForSelectedRow {
+            let buyVC = segue.destination as! BuyViewController
+            buyVC.item = data?[indexPath.item]
+        }
     }
-    */
+ 
 
 }
---
+++ Sell Your Junk/BuyViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BuyViewController.swift
+//  Sell Your Junk
+//
+//  Created by Xavier Triplett on 5/3/19.
+//  Copyright © 2019 Xavier Triplett. All rights reserved.
+//
+
+import UIKit
+
+class BuyViewController: UIViewController {
+
+    var item: Item?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
